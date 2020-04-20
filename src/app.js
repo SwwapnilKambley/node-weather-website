4dfaab8e3299fb98d55ccc00d89d4ae8536642bd
dragonikspidey <dragonikspidey@localhost.localdomain>
2020-04-20 09:51:59 -0400

    Name Change

--- src/app.js
+++ src/app.js
@@ -27,14 +27,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Swwapnil Kambley'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Swwapnil Kambley'
     })
 })
 
@@ -42,7 +42,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Swwapnil Kambley'
     })
 })
 
@@ -91,7 +91,7 @@ app.get('/products', (req,res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Swwapnil Kambley',
         errorMessage: 'Help article not found.'
     })
 })
@@ -99,7 +99,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Swwapnil Kambley',
         errorMessage: 'Page not found.'
     })
 })
